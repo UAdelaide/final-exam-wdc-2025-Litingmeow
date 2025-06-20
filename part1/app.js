8cea91112f39646c16c5204efc4ac42ecf8fe742
Litingmeow <a1916983@adelaide.edu.au>
2025-06-20 12:31:33 +0000

    [automatic commit] 2025-06-20 12:31:33 UTC 1.147.65.239

--- part1/app.js
+++ part1/app.js
@@ -88,11 +88,11 @@ let db;
 // Route to return it as JSON
 app.get('/', async (req, res) => {
   try {
-    const [dogs] = await db.execute('
+    const [dogs] = await db.execute(`
       SELECT Dogs.dog_id, Dogs.name, Dogs.size, Users.username AS owner
       FROM Dogs
       JOIN Users ON Dogs.owner_id = Users.user_id
-      ');
+      `);
     res.json(dogs);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch dogs' });
