156fd0fa4a4eb8096f8d1d90dfd0240e9cdf76a4
Litingmeow <a1916983@adelaide.edu.au>
2025-06-20 12:41:44 +0000

    [automatic commit] 2025-06-20 12:41:44 UTC 1.147.65.239

--- part1/app.js
+++ part1/app.js
@@ -1,11 +1,15 @@
 var express = require('express');
-
-
-const dogsRouter = require('./routes/dogs');
-const walkersRouter = require('./routes/walkers');
-const walkRequestsRouter = require('./routes/walkrequests');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require('mysql2/promise');
 
 var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/dogs', dogsRouter);
 app.use('/api/walkers', walkersRouter);
