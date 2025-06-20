a04467bf9305e49c6a908191c7889f22cf7a1b37
Litingmeow <a1916983@adelaide.edu.au>
2025-06-20 12:34:03 +0000

    [automatic commit] 2025-06-20 12:34:03 UTC 1.147.65.239

--- part1/routes/walkrequests.js
+++ part1/routes/walkrequests.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+const { db } = require('../app');
 
 router.get('/', async (req, res) => {
   try {
