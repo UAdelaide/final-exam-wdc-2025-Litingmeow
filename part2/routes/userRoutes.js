3cd9cbaefc58418ac2cb9442de8a708ee31c0edc
Litingmeow <a1916983@adelaide.edu.au>
2025-06-21 01:32:30 +0000

    [automatic commit] 2025-06-21 01:32:29 UTC 1.147.65.239

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
+const session = require('express-session');
+const bcrypt = require('bcrypt'); // Assuming bcrypt is used for password hashing
 
 // GET all users (for admin/testing)
 router.get('/', async (req, res) => {
